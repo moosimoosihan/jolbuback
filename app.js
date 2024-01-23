9c2309df5df34965124583b7d4366bd9a812dc32
segun <segun408@gmail.com>
2024-01-23 23:35:53 +0900

    키 삭제

--- app.js
+++ app.js
@@ -43,18 +43,7 @@ server.listen(3001, () => {
 })
 
 // 주식 소캣
-const payload = {
-    access_key: "ixEvMaCGj5nRdP0Va5qSGXQmBRe4R7BatWNRS8qw",
-    nonce: uuidv4(),
-};
-
-const jwtToken = jwt.sign(payload, "vOhCXONVCipBvstgqcF2QkH4o7SyQkS4W4LViLeN");
-
-const ws = new WebSocket("wss://api.upbit.com/websocket/v1", {
-    headers: {
-        authorization: `Bearer ${jwtToken}`
-    }
-});
+const ws = new WebSocket("wss://api.upbit.com/websocket/v1");
 
 ws.on("open", () => {
     ws.send(`[{"ticket": "test example"},{"type": "ticker", "codes": ["KRW-BTC", "KRW-ETH"]},{"format": "DEFAULT"}]`);
