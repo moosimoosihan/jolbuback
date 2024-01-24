0fc2ba6b9b88fe4190700fd9e5dd3e01802a89d2
segun <segun408@gmail.com>
2024-01-25 08:58:26 +0900

    주식 상세 불러오기

--- sql.js
+++ sql.js
@@ -3,4 +3,5 @@ module.exports = {
     insert_stock: `INSERT INTO tb_stock(code, opening_price, high_price, low_price, trade_price) values(?, ?, ?, ?, ?)`,
     update_stock: `UPDATE tb_stock SET opening_price=?, high_price=?, low_price=?, trade_price=? WHERE code=?`,
     check_stock: `SELECT * FROM tb_stock WHERE code=?`,
+    stock_info: `SELECT code, opening_price, high_price, low_price, trade_price FROM tb_stock WHERE code=?`,
 }
